de48733c80c5988f60b2513c3102a8e2feb08658
Chingis Oinar <chingisoinar@gmail.com>
2021-02-04 08:11:05 +0900

    feat: softmax + crossEntropy loss is more efficient for backprop

--- Chapter 9 - Backpropagation/losses.py
+++ Chapter 9 - Backpropagation/losses.py
@@ -15,7 +15,7 @@ class Loss:
         
         return data_loss
     def __call__(self, output, y):
-        return self.calculat(output,y)
+        return self.calculate(output,y)
     
 class CatCrossentropy(Loss):
     def forward(self, y_pred, y_true):
@@ -43,3 +43,31 @@ class CatCrossentropy(Loss):
         self.dinputs = -self.y_true / dvalues
         # Normalize gradient
         self.dinputs = self.dinputs / samples
+class Activation_Softmax_Loss_CategoricalCrossentropy():
+    def __init__(self):
+        self.activation = Softmax()
+        self.loss = CatCrossentropy()
+        
+    #forward pass
+    def forward(self, inputs, y_true):
+        #output layer's activation function
+        self.activation.forward(inputs)
+        #set the output
+        self.output = self.activation.output
+        
+        return self.loss.calculate(self.output, y_true)
+    
+    #Backward pass
+    def backward(self, dvalues, y_true):
+        #number of samples
+        samples = len(dvalues)
+        #if labels are one-hot encoded, turn them into discrete values
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+        #copy so we can safely modify
+        self.dinputs = dvalues.copy()
+        #yhat_i_k - y_i_k
+        self.dinputs[range(samples),y_true] -= 1
+        #normalize gradient
+        self.dinputs /= samples
+        
